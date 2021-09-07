1101696ff669b8c92c123b160b138378c17b5c3c
damontoth <Damon.Toth@hsc.utah.edu>
2021-09-07 11:40:23 -0600

    Update Table2.R

--- Table2.R
+++ Table2.R
@@ -4,67 +4,87 @@ dataFileName <- 'analysisData.txt'
 source('prepareData.R')
 source('MLEfunctions.R')
 
-getSolDcInf <- function(piVfix) getSol(c(pCom=0.01,pHouse=0.1,dHouse=1,phiV=0.3,phiA=0.8,piA=0.995),
-							      function(x) -getLL(x[1],Inf,x[2],x[3],x[4],x[5],piVfix,x[6]))
+paramInit <- c(pCom=0.01, pHouse=0.1, dHouse=1, phiV=0.3, phiA=0.8, piV=0.995, piA=0.995)
 
-piVsolDcInf <- optimize(function(x) getSolDcInf(x)$value,c(0.999,1),tol=1e-10)$minimum
-solDcInf <- getSolDcInf(piVsolDcInf)
-solDcInf$par <- c(solDcInf$par[1:5],piV=piVsolDcInf,solDcInf$par[6])
+getModelResults <- function(fixedParams){
+	optParams <- setdiff(names(paramInit),names(fixedParams))
+	
+	optFn <- function(x) -getLL(x['pCom'], Inf, x['pHouse'], x['dHouse'], x['phiV'], x['phiA'], x['piV'], x['piA'])
 
-getSolDcInfDh0 <- function(piV) getSol(c(pCom=0.01,pHouse=0.1,phiV=0.3,phiA=0.8,piA=0.995),
-				    function(x) -getLL(x[1],Inf,x[2],0,x[3],x[4],piV,x[5]))
+	if('piV' %in% names(fixedParams)){
+		sol <- getSol(paramInit[optParams], function(x) optFn(c(x, fixedParams)))
+	}else{
+		getModelSol <- function(piV) getSol(paramInit[setdiff(optParams,'piV')],
+								function(x) optFn(c(x, fixedParams, piV=piV)))
+		piVsol <- optimize(function(x) getModelSol(x)$value, c(0.998,1), tol=1e-10)$minimum
+		sol <- getModelSol(piVsol)
+		solPiV1 <- getModelSol(1)
+		if(solPiV1$value < sol$value){
+			piVsol <- 1
+			sol <- solPiV1
+		}
 
-piVsolDcInfDh0 <- optimize(function(x) getSolDcInfDh0(x)$value,c(0.999,1),tol=1e-10)
-solDcInfDh0 <- getSolDcInfDh0(piVsolDcInfDh0$minimum)
-solDcInfDh0$par <- c(solDcInfDh0$par[1:4],piV=piVsolDcInfDh0$minimum,solDcInfDh0$par[5])
+		sol$par <- c(sol$par,piV=piVsol)
+	}
+	sol$par <- c(sol$par,fixedParams)[names(paramInit)]
+	sol
+}
+
+fixP <- vector('list',18)
+modelNames <- rep('',18)
+
+modelNames[1] <- 'none'; 					fixP[[1]] <- {}
+modelNames[2] <- 'dH=0';					fixP[[2]] <- c(dHouse=0)
+modelNames[3] <- 'dH=Inf';					fixP[[3]] <- c(dHouse=Inf)
+modelNames[4] <- 'piV=1';					fixP[[4]] <- c(piV=1)
+modelNames[5] <- 'phiA=0.893';				fixP[[5]] <- c(phiA=0.893)
+modelNames[6] <- 'piA=0.996';					fixP[[6]] <- c(piA=0.996)
+modelNames[7] <- 'piA=1';					fixP[[7]] <- c(piA=1)
+modelNames[8] <- 'dH=0;piV=1';				fixP[[8]] <- c(dHouse=0,piV=1)
+modelNames[9] <- 'dH=0;phiA=0.893';				fixP[[9]] <- c(dHouse=0,phiA=0.893)
+modelNames[10] <- 'dH=0;piA=0.996';				fixP[[10]] <- c(dHouse=0,piA=0.996)
+modelNames[11] <- 'piV=1;phiA=0.893';			fixP[[11]] <- c(piV=1, phiA=0.893)
+modelNames[12] <- 'piV=1;piA=0.996';			fixP[[12]] <- c(piV=1, piA=0.996)
+modelNames[13] <- 'phiA=0.893;piA=0.996';			fixP[[13]] <- c(phiA=0.893, piA=0.996)
+modelNames[14] <- 'dH=0;piV=1;phiA=0.893';		fixP[[14]] <- c(dHouse=0, piV=1, phiA=0.893)
+modelNames[15] <- 'dH=0;piV=1;piA=0.996';			fixP[[15]] <- c(dHouse=0, piV=1, piA=0.996)
+modelNames[16] <- 'dH=0;phiA=0.893;piA=0.996';		fixP[[16]] <- c(dHouse=0, phiA=0.893, piA=0.996)
+modelNames[17] <- 'phiA=0.893;piV=1;piA=0.996';		fixP[[17]] <- c(phiA=0.893, piV=1, piA=0.996)
+modelNames[18] <- 'dH=0;phiA=0.893;piV=1;piA=0.996';	fixP[[18]] <- c(dHouse=0, phiA=0.893, piV=1, piA=0.996)
 
 
-solDcInfPiV1 <- getSol(c(pCom=0.01,pHouse=0.1,dHouse=1,phiV=0.3,phiA=0.8,piA=0.995), 
-			     function(x) -getLL(x[1],Inf,x[2],x[3],x[4],x[5],1,x[6]))
+getPval <- function(LL, optLL, df) pchisq(2*(optLL-LL), df=df, lower.tail=FALSE)
+getAIC <- function(LL, numParam) 2*(numParam - LL)
 
-solDcInfDhInf <- getSol(c(pCom=0.01,pHouse=0.1,phiV=0.3,phiA=0.8,piV=0.995,piA=0.995), 
-				function(x) -getLL(x[1],Inf,x[2],Inf,x[3],x[4],x[5],x[6]))
+freeParams <- rep(NA,length(modelNames))
+table2opt <- matrix(NA,length(modelNames),7)
+logLik <- rep(NA,length(modelNames))
+pVal <- rep(NA,length(modelNames))
+aic <- rep(NA,length(modelNames))
 
-solDcInfDh0PiV1 <- getSol(c(pCom=0.01,pHouse=0.1,phiV=0.3,phiA=0.8,piA=0.995),
-				  function(x) -getLL(x[1],Inf,x[2],0,x[3],x[4],1,x[5]))
+for(i in seq_along(modelNames)){
+	freeParams[i] <- 7 - length(fixP[[i]])
+	sol <- getModelResults(fixP[[i]])
+	table2opt[i,] <- sol$par
+	logLik[i] <- -sol$value
+	if(i==1){
+		optLL <- logLik[1]
+	}else{
+		pVal[i] <- getPval(logLik[i], optLL, length(fixP[[i]]))
+	}
+	aic[i] <- getAIC(logLik[i], freeParams[i])
+}
 
-solDcInfPiA1PiV1 <- getSol(c(pCom=0.01,pHouse=0.1,dHouse=1,phiV=0.3,phiA=0.8), 
-			         function(x) -getLL(x[1],Inf,x[2],x[3],x[4],x[5],1,1))
+colnames(table2opt) <- names(paramInit)
 
-getSolDcInfPiA1 <- function(piV) getSol(c(pCom=0.01,pHouse=0.1,dHouse=1,phiV=0.3,phiA=0.8),
-			    function(x) -getLL(x[1],Inf,x[2],x[3],x[4],x[5],piV,1))
-
-#Applying getSolDcInfPiA1 for any piV<1 produces lower likelihood than piV=1, so: 
-solDcInfPiA1 <- solDcInfPiA1PiV1
-solDcInfPiA1$par <- c(solDcInfPiA1$par,piV=1)
-
-getSolDcInfPiA996 <- function(piV) getSol(c(pCom=0.01,pHouse=0.1,dHouse=1,phiV=0.3,phiA=0.8),
-			    function(x) -getLL(x[1],Inf,x[2],x[3],x[4],x[5],piV,0.996))
-
-piVsolDcInfPiA996 <- optimize(function(x) getSolDcInfPiA996(x)$value,c(0.999,1))
-solDcInfPiA996 <- getSolDcInfPiA996(piVsolDcInfPiA996$minimum)
-solDcInfPiA996$par <- c(solDcInfPiA996$par,piV=piVsolDcInfPiA996$minimum)
-
-PvalDcInfDh0 <- (1-pchisq(2*(solDcInfDh0$value-solDcInf$value), df=1))
-PvalDcInfDhInf <- (1-pchisq(2*(solDcInfDhInf$value-solDcInf$value), df=1))
-PvalDcInfPiV1 <- (1-pchisq(2*(solDcInfPiV1$value-solDcInf$value),df=1))
-PvalDcInfPiA1 <- (1-pchisq(2*(solDcInfPiA1$value-solDcInf$value),df=1))
-PvalDcInfPiA996 <- (1-pchisq(2*(solDcInfPiA996$value-solDcInf$value),df=1))
-PvalDcInfPiA1PiV1 <- (1-pchisq(2*(solDcInfPiA1PiV1$value-solDcInf$value), df=2))
-
-table2opt <- rbind(solDcInf$par,
-			 c(solDcInfDh0$par[1:2],dHouse=0,solDcInfDh0$par[3:6]),
-			 c(solDcInfDhInf$par[1:2],dHouse=Inf,solDcInfDhInf$par[3:6]),
-			 c(solDcInfPiV1$par[1:5],piV=1,solDcInfPiV1$par[6]),
-			 c(solDcInfPiA996$par,piA=0.996),
-			 c(solDcInfPiA1$par,piA=1))
+deltaAIC <- aic-aic[1]
 
 table2 <- cbind(table2opt,
-		    logLik = -c(solDcInf$value,solDcInfDh0$value,solDcInfDhInf$value,
-				    solDcInfPiV1$value,solDcInfPiA996$value,solDcInfPiA1$value),
-		    pValue = c(NA,PvalDcInfDh0,PvalDcInfDhInf,PvalDcInfPiV1,
-				   PvalDcInfPiA996,PvalDcInfPiA1))
+		    logLik = logLik,
+		    Pvalue = pVal,
+		    freeParams = freeParams,
+		    deltaAIC = deltaAIC)
 
-rownames(table2) <- c('none','dH=0','dH=Inf','piV=1','piA=0.996','piA=1')
+rownames(table2) <- modelNames
 
 print(table2)
